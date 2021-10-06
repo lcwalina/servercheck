2d5a4c1458bc459ed16d79952eaad6849b15de10
lcwalina <lukasz.cwalina.0@gmail.com>
2021-10-06 14:05:01 +0000

    cli correction

--- servercheck/cli.py
+++ servercheck/cli.py
@@ -1,11 +1,16 @@
 import click
 import json
 from .req import ping_servers as ping
+import sys
 
 @click.command()
 @click.option("--json-file", "-f", help="Path to json input file")
 @click.option("--host", "-s", multiple=True, help="host:port to send the request to")
 def cli(host=None, json_file=None):
+    if not(host and json_file):
+        print("This CLI requires arguments to run. See --help for more information")
+        sys.exit(1)
+
     if json_file:
         try:
             with open(json_file, "r") as f:
